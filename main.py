a5433f9dcc9560918e2b068d7e2299f5653ca8d4
athaun <asher.haun@outlook.com>
2021-10-21 20:37:31 -0500

    wakeup light

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import jsonify
 import RPi.GPIO as GPIO
 import schedule
 import time
-
+from multiprocessing import Process, Value
 
 app = Flask(__name__)
 
@@ -14,6 +14,8 @@ GPIO.output(23, GPIO.HIGH)
 lightOn = False
 
 def switchLight ():
+    global lightOn
+    lightOn = (not lightOn)
     if lightOn:
         print("The light is now on")
         GPIO.output(23, GPIO.HIGH)
@@ -21,29 +23,42 @@ def switchLight ():
         print("The light is now off")
         GPIO.output(23, GPIO.LOW)
 
+def turnLightOn ():
+    global lightOn
+    lightOn = True
+    print("The light has been turned on by a scheduled job.")
+    switchLight()
+
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    global lightOn
-    print(request.method)
-
     if request.method == 'POST':
         if request.get_data() == b'lightSwitch':
-            lightOn = (not lightOn)
             switchLight()
-        else:
-            return render_template("index.html", css="static/css/index.css", favicon="static/images/favicon.png", on="static/images/light-on.png", off="static/images/light-off.png", lightOn=lightOn)
     return render_template("index.html", css="static/css/index.css", favicon="static/images/favicon.png", on="static/images/light-on.png", off="static/images/light-off.png", lightOn=lightOn)
 
 @app.route('/_stuff', methods=['GET'])
 def stuff():
     return jsonify(lightOn=lightOn)  
 
+def scheduleJobsAndLoop ():
+    schedule.every().day.at("07:30").do(turnLightOn)
+
+    print("")
+    print(schedule.get_jobs())
+    print("")
+
+    while True:
+        n = schedule.idle_seconds()
+        if n is None:
+            # no more jobs
+            break
+        elif n > 0:
+            # sleep exactly the right amount of time
+            time.sleep(n)
+        schedule.run_pending()
+
 if __name__ == '__main__':
+    p = Process(target=scheduleJobsAndLoop)
+    p.start()
     app.run(host="192.168.1.20")
-
-
-def turnLightOn ():
-    lightOn = True
-    switchLight()
-
-schedule.every().day.at("7:30").do(turnLightOn)
+    p.join()
